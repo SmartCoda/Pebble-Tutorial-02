321e966eca7f61dc279a79fdf35c91cb175d4768
SmartCoda <SmartCoda@users.noreply.github.com>
2015-02-26 08:13:59 +0000

    DOS font

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
   // Get a tm structure
@@ -26,7 +27,7 @@ static void update_time() {
 
 static void main_window_load(Window *window) {
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
@@ -38,11 +39,20 @@ static void main_window_load(Window *window) {
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+  
   // Make sure the time is displayed from the start
   update_time();
 }
 
 static void main_window_unload(Window *window) {
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+  
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
 }
